260938323dfd4ce0f50829eb9c60382e77b54709
atcsol <atcsol@ubuntu>
2025-04-09 01:40:55 +0200

    first commit

---
+++ edroom_ej_nexys_project_23_24/Debug/components/ccledmng/src/CCLEDMngB.d
@@ -0,0 +1,41 @@
+components/ccledmng/src/CCLEDMngB.o components/ccledmng/src/CCLEDMngB.o:  \
+ ../components/ccledmng/src/CCLEDMngB.cpp \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/components/ccledmng/include/public/ccledmng_iface_v1.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroomsl/include/public/edroomsl_iface_v1.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroomsl/include/edroomsl/edroomsl.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/config/include/public/config.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/rtems_osswr/include/public/basic_types.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/rtems_osswr/include/rtems_osswr/leon3_types.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroombp/include/public/edroombp.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroombp/include/edroombp_swr/rtemsapi_4_6/rtems_4_6/edroombp.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroombp/include/edroombp_swr/edroomtypes.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroomsl/include/edroomsl/edroomtc.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroomsl_types/include/public/edroomsl_types.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroomsl_types/include/edroomsl_types/edroomsl_types.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/nexys_gpio_drv/include/public/nexys_gpio_drv_v1.h
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/components/ccledmng/include/public/ccledmng_iface_v1.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroomsl/include/public/edroomsl_iface_v1.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroomsl/include/edroomsl/edroomsl.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/config/include/public/config.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/rtems_osswr/include/public/basic_types.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/rtems_osswr/include/rtems_osswr/leon3_types.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroombp/include/public/edroombp.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroombp/include/edroombp_swr/rtemsapi_4_6/rtems_4_6/edroombp.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroombp/include/edroombp_swr/edroomtypes.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroomsl/include/edroomsl/edroomtc.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroomsl_types/include/public/edroomsl_types.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroomsl_types/include/edroomsl_types/edroomsl_types.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/nexys_gpio_drv/include/public/nexys_gpio_drv_v1.h:
---
+++ edroom_ej_nexys_project_23_24/Debug/examplenexys_mmesp_project.d
@@ -0,0 +1,59 @@
+examplenexys_mmesp_project.o examplenexys_mmesp_project.o:  \
+ ../examplenexys_mmesp_project.cpp \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroom_glue/include/public/edroom_glue.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroom_glue/include/edroom_glue/edroomdf.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroom_glue/include/edroom_glue/edroomdeployment.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroomsl/include/public/edroomsl_iface_v1.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroomsl/include/edroomsl/edroomsl.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/config/include/public/config.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/rtems_osswr/include/public/basic_types.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/rtems_osswr/include/rtems_osswr/leon3_types.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroombp/include/public/edroombp.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroombp/include/edroombp_swr/rtemsapi_4_6/rtems_4_6/edroombp.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroombp/include/edroombp_swr/edroomtypes.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroomsl/include/edroomsl/edroomtc.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroomsl_types/include/public/edroomsl_types.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroomsl_types/include/edroomsl_types/edroomsl_types.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/components/ccsystemmng/include/public/ccsystemmng_iface_v1.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/nexys_gpio_drv/include/public/nexys_gpio_drv_v1.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/leon3_uart_drv/include/public/leon3_uart_drv_v1.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/components/ccledmng/include/public/ccledmng_iface_v1.h \
+  /home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/components/ccdisplaysmng/include/public/ccdisplaysmng_iface_v1.h
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroom_glue/include/public/edroom_glue.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroom_glue/include/edroom_glue/edroomdf.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroom_glue/include/edroom_glue/edroomdeployment.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroomsl/include/public/edroomsl_iface_v1.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroomsl/include/edroomsl/edroomsl.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/config/include/public/config.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/rtems_osswr/include/public/basic_types.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/rtems_osswr/include/rtems_osswr/leon3_types.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroombp/include/public/edroombp.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroombp/include/edroombp_swr/rtemsapi_4_6/rtems_4_6/edroombp.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroombp/include/edroombp_swr/edroomtypes.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroomsl/include/edroomsl/edroomtc.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroomsl_types/include/public/edroomsl_types.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/edroomsl_types/include/edroomsl_types/edroomsl_types.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/components/ccsystemmng/include/public/ccsystemmng_iface_v1.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/nexys_gpio_drv/include/public/nexys_gpio_drv_v1.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/leon3_uart_drv/include/public/leon3_uart_drv_v1.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/components/ccledmng/include/public/ccledmng_iface_v1.h:
+
+/home/atcsol/git/fie_obdh_2025/edroom_ej_nexys_project_23_24/components/ccdisplaysmng/include/public/ccdisplaysmng_iface_v1.h:
